9512749e7b9559d0315eaac6e90d72835bba7853
iareuniqe <1062698930@qq.com>
2018-04-04 01:33:43 +0800

    hello world change hello

--- src/com/tech/data/First.java
+++ src/com/tech/data/First.java
@@ -7,6 +7,6 @@ package com.tech.data;
 public class First {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello");
     }
 }
