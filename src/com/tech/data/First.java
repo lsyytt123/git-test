bb753d0a0d611930169d6bc8e1e6e7a838f0a759
iareuniqe <1062698930@qq.com>
2018-04-04 01:53:35 +0800

    add row

--- src/com/tech/data/First.java
+++ src/com/tech/data/First.java
@@ -7,6 +7,7 @@ package com.tech.data;
 public class First {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello ");
+        System.out.println("best");
     }
 }
