23abad8e3ec9271bd29728c53b799fcfc9735001
iareuniqe <1062698930@qq.com>
2018-04-04 00:59:45 +0800

    first push to remote

---
+++ src/com/tech/data/First.java
@@ -0,0 +1,12 @@
+package com.tech.data;
+
+/**
+ * @author xxx_xx
+ * @date 2018/4/4
+ */
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
