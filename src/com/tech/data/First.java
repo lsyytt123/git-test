731fd723a5cd90f73d473c89de5bd8973a72882e
iareuniqe <1062698930@qq.com>
2018-07-10 00:12:30 +0800

    注释掉

--- src/com/tech/data/First.java
+++ src/com/tech/data/First.java
@@ -14,6 +14,6 @@ public class First {
         System.out.println("打印log");
     }
     public void setLog() {
-        System.out.println("修改继续打印log");
+        //System.out.println("修改继续打印log");
     }
 }
